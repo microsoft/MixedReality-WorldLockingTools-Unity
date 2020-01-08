3da9953e5b384de76bec61417a89969a3f084c0e
Mark Finch <mafinc@microsoft.com>
2020-01-08 17:04:33 +0000

    Variable renaming.

--- Assets/WorldLocking.Core/Scripts/SyncLists.cs
+++ Assets/WorldLocking.Core/Scripts/SyncLists.cs
@@ -33,59 +33,59 @@ namespace Microsoft.MixedReality.WorldLocking.Core
             UpdateResource<ItemType, ResourceType> updater,
             DestroyResource<ResourceType> destroyer)
         {
-            int iVis = resources.Count - 1;
-            int iAnc = currentItems.Count - 1;
+            int iRsrc = resources.Count - 1;
+            int iItem = currentItems.Count - 1;
 
-            while (iVis >= 0 && iAnc >= 0)
+            while (iRsrc >= 0 && iItem >= 0)
             {
-                /// If the existing visuals is greater than the current anchor,
-                /// then there is no corresponding current anchor. So delete the visual
-                int comparison = compareIds(currentItems[iAnc], resources[iVis]);
+                /// If the existing resource is greater than the current item,
+                /// then there is no corresponding current item. So delete the resource.
+                int comparison = compareIds(currentItems[iItem], resources[iRsrc]);
                 if (comparison < 0)
                 {
                     /// items id less than resources, means
                     ///    no item for this resource.
                     /// delete the surplus resource.
-                    destroyer(resources[iVis]);
-                    resources.RemoveAt(iVis);
-                    --iVis;
-                    /// Remain on iAnc
+                    destroyer(resources[iRsrc]);
+                    resources.RemoveAt(iRsrc);
+                    --iRsrc;
+                    /// Remain on iItem
                 }
-                /// If the existing visuals is less, then we are missing a visual for the larger current anchors.
+                /// If the existing resource is less, then we are missing a resource for the larger current item.
                 /// Add it now.
                 else if (comparison > 0)
                 {
                     /// items id greater than resources, means
                     ///    for this item, no matching resource.
                     /// create and add.
-                    var item = creator(currentItems[iAnc]);
-                    resources.Insert(iVis + 1, item);
-                    /// Now ca[ianc] <==> ev[ivis+1]. So move on to ca[ianc-1] / ev[ivis];
-                    --iAnc;
+                    var item = creator(currentItems[iItem]);
+                    resources.Insert(iRsrc + 1, item);
+                    /// Now ca[iItem] <==> ev[iRsrc+1]. So move on to ca[iItem-1] / ev[iRsrc];
+                    --iItem;
                 }
                 else
                 {
                     /// item and resource match, just update.
-                    updater(currentItems[iAnc], resources[iVis]);
-                    --iAnc;
-                    --iVis;
+                    updater(currentItems[iItem], resources[iRsrc]);
+                    --iItem;
+                    --iRsrc;
                 }
             }
 
-            // If iVis && iAnc are both less than zero, then we are done.
-            // If iVis < 0 but iAnc >= 0, then we need more visuals created, from iAnc on down.
-            // If iVis >= 0 but iAnc < 0, then from iVis down needs to be deleted.
-            Debug.Assert(iVis < 0 || iAnc < 0);
-            while (iAnc >= 0)
+            // If iRsrc && iItem are both less than zero, then we are done.
+            // If iRsrc < 0 but iItem >= 0, then we need more resources created, from iItem on down.
+            // If iRsrc >= 0 but iItem < 0, then from iRsrc down needs to be deleted.
+            Debug.Assert(iRsrc < 0 || iItem < 0);
+            while (iItem >= 0)
             {
-                resources.Insert(0, creator(currentItems[iAnc]));
-                --iAnc;
+                resources.Insert(0, creator(currentItems[iItem]));
+                --iItem;
             }
-            while (iVis >= 0)
+            while (iRsrc >= 0)
             {
-                destroyer(resources[iVis]);
-                resources.RemoveAt(iVis);
-                --iVis;
+                destroyer(resources[iRsrc]);
+                resources.RemoveAt(iRsrc);
+                --iRsrc;
             }
             Debug.Assert(resources.Count == currentItems.Count);
         }
