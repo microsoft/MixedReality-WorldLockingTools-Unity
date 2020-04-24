7d483c79815626fcd6ec286cd121ff595f9f95ce
Mark Finch <mafinc@microsoft.com>
2020-04-24 13:36:59 +0100

    Comment the sample alignment script.

--- Assets/WorldLocking.Examples/Scripts/AlignSubtree.cs
+++ Assets/WorldLocking.Examples/Scripts/AlignSubtree.cs
@@ -6,30 +6,61 @@ using UnityEngine.Experimental.UIElements.StyleEnums;
 
 namespace Microsoft.MixedReality.WorldLocking.Examples
 {
+    /// <summary>
+    /// Script to use an independent AlignmentManager to align a specific subtree, independent of the rest of the scene.
+    /// </summary>
+    /// <remarks>
+    /// The subtree aligned by this will remain world-locked by the independent global world-locking
+    /// by the WorldLockingManager.
+    /// </remarks>
     public class AlignSubtree : MonoBehaviour
     {
+        #region Internal members
+
+        /// <summary>
+        /// Owned independent AlignmentManager.
+        /// </summary>
         private AlignmentManager alignmentManager = null;
 
+        /// <summary>
+        /// The transform to align. Defaults to this.gameObject.
+        /// </summary>
         public Transform subTree = null;
 
-        private void CheckAlignmentManager()
+        #endregion Internal members
+
+        /// <summary>
+        /// Create the alignmentManager if needed.
+        /// </summary>
+        /// <remarks>
+        /// The AlignmentManager, though mostly independent, does have a dependency on the WorldLockingManager.
+        /// The WorldLockingManager can't be created until Start/OnEnable (whichever comes first). So even
+        /// though the AlignmentManager isn't a Unity derived type, it is still limited on how early it can
+        /// be created.
+        /// </remarks>
+        private void CheckInternalWiring()
         {
             if (alignmentManager == null)
             {
                 alignmentManager = new AlignmentManager(WorldLockingManager.GetInstance());
             }
-        }
-        // Start is called before the first frame update
-        void Start()
-        {
-            CheckAlignmentManager();
             if (subTree == null)
             {
                 subTree = transform;
-            }    
+            }
         }
 
-        // Update is called once per frame
+        /// <summary>
+        /// Check that all internal wiring is complete.
+        /// </summary>
+        void Start()
+        {
+            CheckInternalWiring();
+        }
+
+        /// <summary>
+        /// Prompt the AlignmentManager to compute a new alignment pose, then apply it to the target subtree.
+        /// </summary>
         void Update()
         {
             Debug.Assert(alignmentManager != null);
@@ -45,16 +76,18 @@ namespace Microsoft.MixedReality.WorldLocking.Examples
             subTree.SetGlobalPose(lockedFromPinned);
         }
 
-        // mafinc - should this be in Start()?
+        /// <summary>
+        /// Check that all internal wiring is complete. Assign our independent alignmentManager
+        /// to all space pins beneath us.
+        /// </summary>
         private void OnEnable()
         {
-            CheckAlignmentManager();
+            CheckInternalWiring();
             var spacePins = GetComponentsInChildren<SpacePin>();
             foreach (var pin in spacePins)
             {
                 pin.AlignmentManager = alignmentManager;
             }
-            // mafinc - Should we try a load here?
         }
     }
 }
