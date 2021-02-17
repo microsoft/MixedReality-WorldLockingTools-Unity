949346a2769951380d04d0a14dcc387120bff729
Mark Finch <mafinc@microsoft.com>
2021-02-17 16:33:27 +0000

    Fix (required) changelog.md.

--- pipelines/scripts/upm_pack.cmd
+++ pipelines/scripts/upm_pack.cmd
@@ -9,14 +9,14 @@ rem After each packaging, inspect the results in the spawned window.
 rem If results look good, close the window to proceed to next package.
 pushd ..\..\Assets
 
-xcopy package_core.json package.json /YQ
-xcopy package_core.json.meta package.json.meta /YQ
-echo Close npm window after checking results.
+copy package_core.json package.json /Y
+copy package_core.json.meta package.json.meta /Y
+echo Exit npm window after checking results (enter "exit" on command line).
 start /WAIT npm pack
 
-xcopy package_samples.json package.json /YQ
-xcopy package_samples.json.meta package.json.meta /YQ
-echo Close npm window after checking results.
+copy package_samples.json package.json /Y
+copy package_samples.json.meta package.json.meta /Y
+echo Exit npm window after checking results (enter "exit" on command line).
 start /WAIT npm pack
 
 rem Cleanup.
