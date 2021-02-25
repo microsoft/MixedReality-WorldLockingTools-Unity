366a018fab2bc53cc47b8beebd9d7931ab91e75f
Mark Finch <mafinc@microsoft.com>
2021-02-25 14:23:16 +0000

    One more collision between XRAnchor and XRReferencePoint

--- Assets/WorldLocking.Core/Scripts/XR/AnchorManagerXR_2018.cs
+++ Assets/WorldLocking.Core/Scripts/XR/AnchorManagerXR_2018.cs
@@ -69,10 +69,10 @@ namespace Microsoft.MixedReality.WorldLocking.Core
 
         private static XRReferencePointSubsystem FindReferencePointManager()
         {
-            List<XRReferencePointSubsystem> anchorSubsystems = new List<XRAnchorSubsystem>();
+            List<XRReferencePointSubsystem> anchorSubsystems = new List<XRReferencePointSubsystem>();
             SubsystemManager.GetInstances(anchorSubsystems);
             Debug.Log($"Found {anchorSubsystems.Count} anchor subsystems.");
-            XRAnchorSubsystem activeSubsystem = null;
+            XRReferencePointSubsystem activeSubsystem = null;
             foreach (var sub in anchorSubsystems)
             {
                 if (sub.running)
