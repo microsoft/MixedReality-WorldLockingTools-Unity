ffce495fb93a262622365c9b0879b92c66d88c95
Mark Finch <mafinc@microsoft.com>
2021-03-04 14:11:36 +0000

    Update to MRTK v2.6.0.

---
+++ Assets/MRTK/Tools/RuntimeTools/Tools/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+[assembly: System.Reflection.AssemblyVersion("2.6.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("2.6.0.0")]
+
+[assembly: System.Reflection.AssemblyProduct("Microsoft® Mixed Reality Toolkit Tools")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright © Microsoft Corporation")]
