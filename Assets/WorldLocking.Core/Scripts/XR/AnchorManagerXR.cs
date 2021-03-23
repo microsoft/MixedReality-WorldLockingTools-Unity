63e950c540ce2e31c0f83eab0c97fa8c18da1b3f
Mark Finch <mafinc@microsoft.com>
2021-03-23 16:58:59 +0000

    New startup algorithm for U2020 and beyond, tested and commented.

--- Assets/WorldLocking.Core/Scripts/XR/AnchorManagerXR.cs
+++ Assets/WorldLocking.Core/Scripts/XR/AnchorManagerXR.cs
@@ -78,36 +78,74 @@ namespace Microsoft.MixedReality.WorldLocking.Core
             }
 
             var session = FindSessionSubsystem();
+            if (session == null)
+            {
+                return null;
+            }
 
             AnchorManagerXR anchorManager = new AnchorManagerXR(plugin, headTracker, xrAnchorManager, session);
 
             return anchorManager;
         }
 
+        /// <summary>
+        /// Find the correct AnchorManager for this session.
+        /// </summary>
+        /// <returns>Anchor manager for this session.</returns>
+        /// <remarks>
+        /// For HoloLens, we are looking for the _active_ anchor subsystem. There may be multiple
+        /// anchor subsystems (e.g. OpenXR Plugin and WMR XR Plugin), but only one will be active.
+        /// However, on Android, no anchor subsystem is active until we make it active by calling Start() on it.
+        /// So if we still don't have an active subsystem, try calling start on any that are available and 
+        /// if that makes them active (running), then take that one. 
+        /// Note that this would be bad on HoloLens, as it would start up a subsystem that is installed but not currently selected.
+        /// I believe that iOS works as HoloLens does, but I don't want to rely on undocumented behavior. The algorithm here
+        /// should work on either.
+        /// </remarks>
         private static XRAnchorSubsystem FindAnchorManager()
         {
             List<XRAnchorSubsystem> anchorSubsystems = new List<XRAnchorSubsystem>();
             SubsystemManager.GetInstances(anchorSubsystems);
             Debug.Log($"Found {anchorSubsystems.Count} anchor subsystems.");
             XRAnchorSubsystem activeSubsystem = null;
+            int numFound = 0;
             foreach (var sub in anchorSubsystems)
             {
                 if (sub.running)
                 {
                     Debug.Log($"Found active anchor subsystem {sub.subsystemDescriptor.id}.");
                     activeSubsystem = sub;
+                    ++numFound;
                 }
             }
             if (activeSubsystem == null)
             {
+                Debug.Log($"Found no anchor subsystem running, will try starting one.");
+                foreach (var sub in anchorSubsystems)
+                {
+                    sub.Start();
+                    if (sub.running)
+                    {
+                        activeSubsystem = sub;
+                        ++numFound;
+                        Debug.Log($"Start changed an anchor subsystem to running.");
+                    }
+                }
             }
-            if (activeSubsystem == null)
+            if (numFound != 1)
             {
-                Debug.LogError($"No active anchor subsystem found.");
+                Debug.LogError($"Found {numFound} active anchor subsystems, expected exactly one.");
             }
             return activeSubsystem;
         }
 
+        /// <summary>
+        /// Find the session subsystem for this session.
+        /// </summary>
+        /// <returns>Session subsystem for this session in an active running state.</returns>
+        /// <remarks>
+        /// See remarks in <see cref="FindAnchorManager"/> above.
+        /// </remarks>
         private static XRSessionSubsystem FindSessionSubsystem()
         {
             List<XRSessionSubsystem> sessionSubsystems = new List<XRSessionSubsystem>();
@@ -122,6 +160,20 @@ namespace Microsoft.MixedReality.WorldLocking.Core
                     Debug.Log($"Found active session subsystem");
                     activeSession = session;
                     ++numFound;
+                }
+            }
+            if (activeSession == null)
+            {
+                Debug.Log($"Found no active session subsystem, will try starting one.");
+                foreach (var session in sessionSubsystems)
+                {
+                    session.Start();
+                    if (session.running)
+                    {
+                        activeSession = session;
+                        ++numFound;
+                        Debug.Log($"Start changed a session to running.");
+                    }
                 }
             }
             if (numFound != 1)
