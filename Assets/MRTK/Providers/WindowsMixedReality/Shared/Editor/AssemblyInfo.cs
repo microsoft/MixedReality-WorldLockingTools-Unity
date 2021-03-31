5e05c50d5e20712985ab993606edc4523fa464a8
Mark Finch <mafinc@microsoft.com>
2021-03-31 14:53:49 +0100

    Mrtk2.6.1 (#150)
    * Deleted mrtk v2.6.0 and imported mrtk v2.6.1, Foundation and Tools only.
    
    * Missed a .meta file.

--- Assets/MRTK/Providers/WindowsMixedReality/Shared/Editor/AssemblyInfo.cs
+++ Assets/MRTK/Providers/WindowsMixedReality/Shared/Editor/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-[assembly: System.Reflection.AssemblyVersion("2.6.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("2.6.0.0")]
+[assembly: System.Reflection.AssemblyVersion("2.6.1.0")]
+[assembly: System.Reflection.AssemblyFileVersion("2.6.1.0")]
 
 [assembly: System.Reflection.AssemblyProduct("Microsoft® Mixed Reality Toolkit Providers")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright © Microsoft Corporation")]
