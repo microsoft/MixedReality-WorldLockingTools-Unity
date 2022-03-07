c4aa7c288f3d8692c8f8d473000c45c927add8ae
Mark Finch <mafinc@microsoft.com>
2022-03-07 14:16:06 +0000

    Update to MRTK v2.7.3

--- Assets/MRTK/Providers/WindowsMixedReality/Shared/AssemblyInfo.cs
+++ Assets/MRTK/Providers/WindowsMixedReality/Shared/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Microsoft.MixedReality.Toolkit.Providers.WindowsMixedReality")]
 [assembly: InternalsVisibleTo("Microsoft.MixedReality.Toolkit.Providers.XRSDK.WindowsMixedReality")]
-[assembly: System.Reflection.AssemblyVersion("2.7.2.0")]
-[assembly: System.Reflection.AssemblyFileVersion("2.7.2.0")]
+[assembly: System.Reflection.AssemblyVersion("2.7.3.0")]
+[assembly: System.Reflection.AssemblyFileVersion("2.7.3.0")]
 
 [assembly: System.Reflection.AssemblyProduct("Microsoft® Mixed Reality Toolkit Providers")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright © Microsoft Corporation")]
